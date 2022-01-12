fae1fcf5b75d0217699daec3ae107a4c45995264
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-12 19:26:53 +0100

    Koordináták kész

--- app/src/main/java/petrik/hu/gps/MainActivity.java
+++ app/src/main/java/petrik/hu/gps/MainActivity.java
@@ -1,5 +1,6 @@
 package petrik.hu.gps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -9,9 +10,13 @@ import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.PerformanceHintManager;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +32,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        //Helymeghatározás ellenőrzése
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+            textViewGps.setText("Nincs helymeghatározás engedélyezve");
+
+            //Engedély kérés ablak megnyitása
+            String[] permission = new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                };
+            ActivityCompat.requestPermissions(this, permission, 0);
+            //return;
+        }
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, locationListener
+            );
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 0, 0, locationListener
+                    );
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myTimer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                TimerMethod();
+            }
+        };
+        myTimer.schedule(timerTask, 1000, 5000);
+    }
+
+    private void TimerMethod() {
+        this.runOnUiThread(TimerTick);
+    }
+
+    private final Runnable TimerTick = new Runnable() {
+        @Override
+        public void run() {
+            textViewGps.setText("longitude: " + longitude + "\n" + "latitude: " + latitude);
+            if (writePermission) {
+                try {
+                    Naplozas.kiir(longitude, latitude);
+                } catch (IOException e) {
+                    Log.d("Kiiras: ", e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
     public void init() {
         textViewGps = findViewById(R.id.textViewGps);
